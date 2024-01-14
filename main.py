b14343abac03ad8367d7c7d53fe95db652d8b5f0
kerkpower <kerkpowerfort@gmail.com>
2024-01-14 17:29:56 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,57 @@
+import random
+
+tools = ("rock", "paper", "scissors")
+
+
+def get_input() -> str:
+    move = input("Choose your move: ").lower()
+
+    while not check_input(move):
+        print("The specified move is not correct, try again.")
+        move = input("Choose your move: ").lower()
+
+    return move
+
+
+def check_input(move: str) -> bool:
+    if move not in tools:
+        return False
+
+    return True
+
+
+def random_opponent_tool() -> str:
+    return random.choice(tools)
+
+
+def check_winner(user_move: str, opponent_move: str) -> str:
+    if user_move == opponent_move:
+        return "Tie"
+
+    match user_move:
+        case "rock":
+            if opponent_move == "paper":
+                return "Loss"
+
+        case "paper":
+            if opponent_move == "scissors":
+                return "Loss"
+
+        case "scissors":
+            if opponent_move == "rock":
+                return "Loss"
+
+    return "Win"
+
+
+def main():
+    print("----Rock Paper Scissors----")
+
+    user_move = get_input()
+    random_move = random_opponent_tool()
+
+    print(check_winner(user_move, random_move))
+
+
+if __name__ == "__main__":
+    main()
